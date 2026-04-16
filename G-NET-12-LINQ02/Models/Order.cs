854bc813f3b2948d6d4d13952770e10a1dbff3c8
George Fayek <georgefayekwilliam@gmail.com>
2026-04-16 16:36:47 +0200

    1. Get top 3 most expensive products

---
+++ G-NET-12-LINQ02/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_LINQ02.Models
+{
+    internal class Order
+    {
+        public decimal Total { get; set; }
+    }
+}
--- G-NET-12-LINQ02/Program.cs
+++ G-NET-12-LINQ02/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace G_NET_12_LINQ02
+﻿using G_NET_12_LINQ02.Models;
+namespace G_NET_12_LINQ02
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<Product> products = new List<Product>
+            {
+                new Product{ ProductID=1, ProductName="Chai", UnitPrice=18, UnitsInStock=39, Category="Beverages"},
+                new Product{ ProductID=2, ProductName="Chang", UnitPrice=19, UnitsInStock=17, Category="Beverages"},
+                new Product{ ProductID=3, ProductName="Aniseed Syrup", UnitPrice=10, UnitsInStock=13, Category="Condiments"},
+                new Product{ ProductID=4, ProductName="Chef Anton", UnitPrice=22, UnitsInStock=53, Category="Seafood"},
+                new Product{ ProductID=5, ProductName="Ikura", UnitPrice=31, UnitsInStock=0, Category="Seafood"},
+                new Product{ ProductID=18, ProductName="Carnarvon Tigers", UnitPrice=62, UnitsInStock=42, Category="Seafood"}
+            };
+
+
+            #region 1. Get top 3 most expensive products
+          //  var result1 = products
+          //      .OrderByDescending(p => p.UnitPrice)
+          //      .Take(3);
+
+          //   foreach (var p in result1)
+          //Console.WriteLine(p.ProductName);
+
+            #endregion
+
         }
     }
 }
